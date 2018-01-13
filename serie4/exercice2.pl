f5c6140557d6fe74f65c7e69cf03917836e45d65
Jairo Duarte <alfredojairo17@hotmail.com>
2018-01-13 14:27:50 +0000

    s4 ex2 exemple 1 ok

--- serie4/exercice2.pl
+++ serie4/exercice2.pl
@@ -1,26 +1,32 @@
 :- include("../outils.pl").
 
-op_c(X,Y,Z) :-
-    Z is X*Y.
+op_c(X,Y,Z,W) :-
+    Z is X*Y,
+    W = X*Y.
 
-op_c(X,Y,Z) :- 
-    Z is X+Y.
+op_c(X,Y,Z,W) :- 
+    Z is X+Y,
+    W = X+Y.
 
-op_c(X,Y,Z) :- 
-    Z is X/Y.
+op_c(X,Y,Z,W) :- 
+    Z is X/Y,
+    W = X / Y.
 
-op_c(X,Y,Z) :- 
-    Z is X-Y.
+op_c(X,Y,Z,W) :- 
+    Z is X-Y,
+    W = X - Y.
 
 main([Y],Y).
 main([X|Reste],Q) :-
-    result([X|Reste],R),
-    R =:= Q,!.
+    result([X|Reste],R,K),
+    R =:= Q,
+    write(K),!. 
 
-result([X],X).
-result([X,Y|Reste],Q) :-
-    op_c(X,Y,Z),
-    result([Z|Reste],Q).
+result([X,Y],Z,W) :- op_c(X,Y,Z,W).
+result([X,Y|Reste],Q,I) :-
+    op_c(X,Y,Z,W),
+    result([W|Reste],Q,I).
+    %K = I.
 
 
 %calc([Y],)
