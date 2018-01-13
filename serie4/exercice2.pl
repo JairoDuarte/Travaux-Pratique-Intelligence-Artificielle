dad5d62f14f797f6e4c67c78e179337775ada149
Jairo Duarte <alfredojairo17@hotmail.com>
2018-01-13 14:28:31 +0000

    s4 ex2 exemple 1 ok

--- serie4/exercice2.pl
+++ serie4/exercice2.pl
@@ -26,21 +26,3 @@ result([X,Y],Z,W) :- op_c(X,Y,Z,W).
 result([X,Y|Reste],Q,I) :-
     op_c(X,Y,Z,W),
     result([W|Reste],Q,I).
-    %K = I.
-
-
-%calc([Y],)
-calc([T],_,T).
-calc([X,Y|R], Z, W) :-
-    calc([Y|R], Z,K),
-    op_c(X,Y,F),
-    op_c(F,K,W),
-    Z == W.
-
-
-calc_t([],0).
-calc_t([Y],Y).
-calc_t([X,Y|R],Z) :-
-    op_c(X,Y,Z),
-    calc_t([Y|R],Z),
-    op_c(Z,RS,RF).
