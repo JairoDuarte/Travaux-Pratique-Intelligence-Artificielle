7bfe3b2f495ace0eaba99ab0a532aee6b32ec136
Jairo Duarte <alfredojairo17@hotmail.com>
2018-01-25 18:07:42 +0000

    exercice fait à 80%

--- serie4/exercice5.pl
+++ serie4/exercice5.pl
@@ -1,58 +1,37 @@
 :- include("../outils.pl").
 :- include("../serie3/liste.pl").
-
 /* --------------------- Exemples d'états initial --------- */
-% Eo = [[disque(1),disque(4),disque(4)],[],[]]
-
-% etat_initial([[disque(1),disque(4),disque(5)],[],[]]). 
+% Eo = 
+etat_initial([tour(1,[1,2,3]),tour(2,[]),tour(3,[])]). 
 /* ------------------ Test de but ------------------- */
 
-test_but([[],[],[disque(1),disque(4),disque(5)]]).
+%test_but([tour(1,[]),tour(2,[]),tour(3,[1,2,3])]).
+test_but(Ef) :-
+    membre(tour(3,[1,2,3]),Ef).
 
 /* ------------------- Relation successeur ------------- */
 % succ(+E1,-E2)
 /*------------------------------------------------------*/
-succ([L1,L2,L3],[NL1,NL2,NL3]) :-
-    deplacer(L1,L2,L3,NL1,NL2,NL3).
-    /* L1 = [],
-    deplacer(L1,N,NL1),conc(N,L2,NL2),
-    NL3 = L3;
-    L1 == [], NL1 = [], L2 = [],
-    deplacer(L2,N,NL2),
-    conc(N,L3,NL3). */
+succ(E1,E2) :-
+    suppr(tour(T1,L1),E1,R1),
+    suppr(tour(T2,L2),R1,R3),
+    T1 \= T2,
+    deplacer(L1,L2,NL1,NL2),tour_valide(NL2),
+    setof(tour(K,Z),membre(tour(K,Z),[tour(T1,NL1),tour(T2,NL2)|R3]),E2).
 
-deplacer([X|R],[],[],R,[X],[]). %
-deplacer([X|R],[],L,R,[X],L). %
-deplacer([disque(X)|R],[disque(Y)|R1],[], R,[disque(Y)|R1],[disque(X)]) :- %
-    X > Y.
-deplacer([disque(X)|R],[disque(Y)|R1],[], [disque(Y),disque(X)|R],R1,[]) :- %
-    X > Y.
-deplacer([disque(X)|R],[disque(Y)|R1],[disque(Z)|R2], R,[disque(Y)|R1],[disque(X),disque(Z)|R2]) :-
-    X > Y, X < Z.
+succ(E1,E2,["deplace de la tour ",T1,"par ",T2]) :-
+    suppr(tour(T1,L1),E1,R1),
+    suppr(tour(T2,L2),R1,R3),
+    T1 \= T2,
+    deplacer(L1,L2,NL1,NL2),tour_valide(NL2),
+    setof(tour(K,Z),membre(tour(K,Z),[tour(T1,NL1),tour(T2,NL2)|R3]),E2).
+    %conc([tour(T2,NL2)],R3,Nt),conc([tour(T1,NL1)],Nt,E2).
 
-deplacer([disque(X)|R],[disque(Y)|R1],[disque(Z)|R2], [disque(X)|R],R1,[disque(Y),disque(Z)|R2]) :-
-    X > Y, Y < Z. %
-deplacer([X],[],[disque(Z)|R2],[],[X],[disque(Z)|R2]). %
-deplacer([],L,[Z],[Z],L,[]). %
-%deplacer(L,[Y],[],L,[],[Y]). %
+tour_valide([_]).
+tour_valide([X,Y|_]) :- 
+    number(X),number(Y),X<Y.
 
-deplacer([],[disque(Y)|R1],[disque(Z)|R2],[],R1,[disque(Y),disque(Z)|R2]) :-
-   Z > Y.
-%deplacer([],[disque(Y)|R1],[disque(Z)|R2],[],[disque(Z),disque(Y)|R1],R2) :-
- %    Y < Z. %
-     
-%deplacer([disque(X)|R],[disque(Y)|R1],[disque(Z)|R2],[disque(X)|R],[R1],[disque(Y),disque(Z)|R2]) :-
- %   X < Y,Y<Z.
-%deplacer([X],[],[disque(Z)|R2],[],[],[X,disque(Z)|R2]).
-
-%deplacer([disque(X)|R],[disque(Y)|R1],[],[disque(X)|R],R1,[disque(Y)]) :-
- %   X < Y.
-
-
-dis([L1,L2,L3],L1,L2,L3).
-/* deplacer(+L1,-L2,-L3) : L2 liste avec le premier élément de L1  , L3 liste avec les reste des éléments de L1   */
-deplacer([X],[X],[]).
-deplacer([X|R],[X],R).
+deplacer([X|R],L2,R,[X|L2]).
 
 /*----------------------------------------------------------------------
 	Programme : dfs_id.pl
@@ -86,7 +65,7 @@ it n=2 :  E3 = [2,3,1], E2 = [2,1,3],   E1 = [1,2,3] ; E1 = Ef donc état final 
 
 dfs_id(E0, Chemin_Sol) :-
 	chemin(E0, Ef, Chemin_Sol),
-	test_but(Ef).
+	test_but(Ef).	
 
 chemin(Etat, Etat, [Etat]).		% chemin de Etat vers Etat est [Etat]	
 
@@ -99,6 +78,26 @@ solution_dfs(E0) :-
 	dfs_id(E0, S),
 	inverser(S, Sol),
 	afficher(Sol).
+
+/* Solution avec action */
+dfs_id(E0, Chemin_Sol,Act) :-
+	chemin(E0, Ef, Chemin_Sol,Act),
+	test_but(Ef).
+
+chemin(Etat, Etat, [Etat],[]).
+
+chemin(E1, E2, [E2|Chemin],[Act|ActP]) :-
+	chemin(E1, E3, Chemin,ActP),
+	succ(E3, E2,Act),
+	not(membre(E2, Chemin)).
+
+solution_dfs_a(E0) :-
+    dfs_id(E0, S,Act),
+	inverser(S, Sol),
+	afficher(Sol),
+    write("Actions :"),nl,nl,
+    inverser(Act,A),
+    afficher(A).
 
 /*----------------------------------------------------------------------
 	Programme : bfs.pl
@@ -162,38 +161,44 @@ solution_bfs(E0) :-
 	inverser(S, Sol),
 	afficher(Sol).
 
-
-/*----------------------------------------------------------------------
-	Programme : dfs_ts.pl
-
-Algorithme de Recherche en profondeur sans détection de cycles
-	Depth First Search - Tree Search
-	
-	dfs_ts(Etat_initial, Chemin_Sol) :
-		Chemin_Sol est le chemin de Etat_initial a l'état final
------------------------------------------------------------------------
-Utilise les prédicats correspondants au problème à résoudre suivants :
-	test_but(Etat)
-		est vrai si Etat est l'état final (état but)
-
-	succ(Etat1, Etat2)
-		relation successeur : Etat2 est un successeur de Etat1
- ---------------------------------------------------------------------*/
-solution(E1, Ch_Sol) :-
-	dfs_ts(E1, Ch_Sol).
-
-solution(E1) :-
-	dfs_ts(E1, Ch_Sol),
-    afficher(Ch_Sol).
- 
-dfs_ts(Ef, [Ef]) :-
-	test_but(Ef).
-
-dfs_ts(E1,[E1|Ch1]) :-
-	succ(E1, E2),
-	dfs_ts(E2, Ch1).
-
 /* Test 
+?- etat_initial(E),solution_dfs(E).
+[tour(1,[1,2,3]),tour(2,[]),tour(3,[])]
+[tour(1,[2,3]),tour(2,[]),tour(3,[1])]
+[tour(1,[3]),tour(2,[2]),tour(3,[1])]
+[tour(1,[3]),tour(2,[1,2]),tour(3,[])]
+[tour(1,[]),tour(2,[1,2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[]),tour(3,[2,3])]
+[tour(1,[]),tour(2,[]),tour(3,[1,2,3])]
 
+?- etat_initial(E),solution_bfs(E).
+[tour(1,[1,2,3]),tour(2,[]),tour(3,[])]
+[tour(1,[2,3]),tour(2,[]),tour(3,[1])]
+[tour(1,[3]),tour(2,[2]),tour(3,[1])]
+[tour(1,[3]),tour(2,[1,2]),tour(3,[])]
+[tour(1,[]),tour(2,[1,2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[]),tour(3,[2,3])]
+[tour(1,[]),tour(2,[]),tour(3,[1,2,3])]
+
+?- etat_initial(E),solution_dfs_a(E).
+[tour(1,[1,2,3]),tour(2,[]),tour(3,[])]
+[tour(1,[2,3]),tour(2,[]),tour(3,[1])]
+[tour(1,[3]),tour(2,[2]),tour(3,[1])]
+[tour(1,[3]),tour(2,[1,2]),tour(3,[])]
+[tour(1,[]),tour(2,[1,2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[2]),tour(3,[3])]
+[tour(1,[1]),tour(2,[]),tour(3,[2,3])]
+[tour(1,[]),tour(2,[]),tour(3,[1,2,3])]
+Actions :
+
+[deplace de la tour ,1,par ,3]
+[deplace de la tour ,1,par ,2]
+[deplace de la tour ,3,par ,2]
+[deplace de la tour ,1,par ,3]
+[deplace de la tour ,2,par ,1]
+[deplace de la tour ,2,par ,3]
+[deplace de la tour ,1,par ,3]
 
 */
